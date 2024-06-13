34bb9f882d66a9bd2b9ff4fdb7ae96c075bcd91b
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-06-13 20:33:37 +0300

    task-1

--- js/js-1-categories.js
+++ js/js-1-categories.js
@@ -7,16 +7,6 @@ categoriesItemsRefs.forEach((categoriesItemRef) => {
   const categoryTitleRef = categoriesItemRef.querySelector("h2");
   const categoryItemsRefs = categoriesItemRef.querySelectorAll("li");
 
-  console.log(
-    `Category: ${categoryTitleRef.textContent}, Number of items: ${categoryItemsRefs.length}`
-  );
+  console.log(`Category: ${categoryTitleRef.textContent}`);
+  console.log(`Elements: ${categoryItemsRefs.length}`);
 });
-
-
-// Output: 
-
-
-Number of categories: 3
-Category: Animals, Number of items: 4
-Category: Products, Number of items: 3
-Category: Technologies, Number of items: 5
