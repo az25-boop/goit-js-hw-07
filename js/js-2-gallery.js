63c35bfe781441801e80eff8425658f47833ce6b
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-06-14 07:07:48 +0300

    js

--- js/js-2-gallery.js
+++ js/js-2-gallery.js
@@ -1,10 +1,47 @@
-const galleryRef = document.querySelector(".gallery");
+// Зображення для галереї
+const images = [
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    alt: "Alpine Spring Meadows",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    alt: "Nature Landscape",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    alt: "Lighthouse Coast Sea",
+  },
+];
 
-const imagesMarkup = images.reduce(
-  (markup, { url, alt }) =>
-    markup +
-    `<li><img src="${url}" alt="${alt}" width="400"></li>`,
-  ""
-);
+// Отримання посилання на галерею
+const gallery = document.querySelector(".gallery");
 
-galleryRef.insertAdjacentHTML("beforeend", imagesMarkup);
+// Створення фрагменту для оптимізації додавання елементів
+const fragment = document.createDocumentFragment();
+
+// Додавання зображень до галереї за одну операцію
+images.forEach((image) => {
+  const li = document.createElement("li");
+  li.classList.add("gallery-item");
+  const img = document.createElement("img");
+  img.src = image;
+  img.alt = "Gallery Image";
+  li.appendChild(img);
+  fragment.appendChild(li);
+});
+
+// Додавання фрагменту з елементами до галереї
+gallery.appendChild(fragment);
